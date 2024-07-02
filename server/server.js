89b340988a600997ffb55dc82b271e421210a01f
Jorge Duarte <sadujorge@gmail.com>
2024-07-01 18:36:38 -0700

    Added get user favorite end point.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require("path");
 const db = require("./config/connection");
+const { User } = require("./models");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -9,6 +10,19 @@ app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('/api/favorites', async (req, res) => {
+    try {
+        const user = await User.findOne({ username: 'brayan' }); 
+        if (user) {
+            res.json(user.favorites);
+        } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching data', error });
+    }
+});
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
